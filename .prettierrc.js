52f0afb65f6df08e4970056c9f111c9b0200e72e
Colten Van Tussenbrook <coltenvantussenbrook@gmail.com>
2023-10-14 15:36:05 -0600

    eslint and prettier configuration

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    semi: true,
+    singleQuote: true,
+    tabWidth: 2,
+    printWidth: 140,
+    trailingComma: 'all',
+    arrowParens: 'always',
+    bracketSpacing: true,
+    proseWrap: 'preserve',
+};
